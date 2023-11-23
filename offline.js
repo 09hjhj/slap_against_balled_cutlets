ece44d82e974668e152cfa06608a4ee894014fdd
floppa728374 <150988840+09hjhj@users.noreply.github.com>
2023-11-23 09:17:45 +0500

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,21 @@
+﻿{
+	"version": 1700713012,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/ПлиточныйФон.png",
+		"images/Спрайт-sheet0.png",
+		"images/ракета-sheet0.png",
+		"images/Спрайт3-sheet0.png",
+		"images/Спрайт2-sheet0.png",
+		"images/Частицы.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
